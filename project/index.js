7215526d575f6d1ed610e62b6878c89b310d10f1
Yamen201005 <yamenyaseen60@gmail.com>
2025-07-12 12:42:50 +0300

    working

---
+++ project/index.js
@@ -0,0 +1,29 @@
+function showflex() {
+    const countainer = document.getElementById("flex-countainer");
+    countainer.style.display ="flex";
+}
+function showp2(){
+    let show1 =document.getElementById("p2")
+    show1.style.display="flex"
+}
+function showp3(){
+    let show2 =document.getElementById("p3")
+    show2.style.display="flex"
+}
+
+function showp1() {
+    const p1 =document.getElementById("p1")
+    p1.style.display ="flex"
+}
+function hidep1(){
+    let hiddenelement =document.getElementById("p1")
+    hiddenelement.style.display="none"
+}
+function hidep2(){
+    let hiddenelement2 =document.getElementById("p2")
+    hiddenelement2.style.display="none"
+}
+function hidep3(){
+    let hiddenelement =document.getElementById("p3")
+    hiddenelement33.style.display="none"
+}
